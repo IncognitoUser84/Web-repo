6f05495cac61b174e9b02416c5886f229877bb13
IncognitoUser84 <89994947+SiyangLiu-CTD@users.noreply.github.com>
2021-10-19 23:40:34 -0600

    Update index.js

--- wk7/index.js
+++ wk7/index.js
@@ -3,10 +3,10 @@ const quotes = ["Be yourself; everyone else is already taken.",
 "I'm selfish, impatient and a little insecure. I make mistakes, I am out of control and at times hard to handle. But if you can't handle me at my worst, then you sure as hell don't deserve me at my best.",
 "Two things are infinite: the universe and human stupidity; and I'm not sure about the universe.",
 "So many books, so little time.",
-"A room without books is like a body without a soul"]
+"A room without books is like a body without a soul"];
 let quotecount = 0;
 
-document.getElementById("newquote").addEventListener("click",buttonClicked)
+document.getElementById("newquote").addEventListener("click",buttonClicked);
 
 function buttonClicked(){
   console.log(quotes[quotecount]);
