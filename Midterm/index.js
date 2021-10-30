cfbfb8b8e2ee5718f9e257e6890d19cf7da0f473
IncognitoUser84 <89994947+SiyangLiu-CTD@users.noreply.github.com>
2021-10-29 19:54:50 -0600

    s

--- Midterm/index.js
+++ Midterm/index.js
@@ -3,5 +3,22 @@ days = ["Mon","Tue","Wed","Thu","Fri","Sat","Sun"];
 for(i=0; i<document.getElementsByClassName("day").length; i++){
   console.log(days[i]);
   document.getElementsByClassName("day")[i].innerHTML = days[i];
+  document.getElementsByClassName("day")[i].addEventListener('click', setActive,false);
   document.getElementsByClassName("day")[i].id = days[i];
+}
+
+function setActive(e){
+  console.log('clicked');
+  e.target.classList.toggle('active');
+  console.log(e.target.innerHTML);
+
+  if(e.target.tagName == "H2"){
+    console.log("h2 clicked");
+    e.target.parentNode.classList.toggle("active");
+
+  }else{
+    console.log("div clicked");
+    e.target.classList.toggle("active");
+  }
+
 }
