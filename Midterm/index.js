1a7042108f7d5b2505b00b9c9b5e42204218af73
IncognitoUser84 <89994947+SiyangLiu-CTD@users.noreply.github.com>
2021-10-29 18:55:18 -0600

    s

--- Midterm/index.js
+++ Midterm/index.js
@@ -3,5 +3,5 @@ days = ["Mon","Tue","Wed","Thu","Fri","Sat","Sun"];
 for(i=0; i<document.getElementsByClassName("day").length; i++){
   console.log(days[i]);
   document.getElementsByClassName("day")[i].innerHTML = days[i];
-
+  document.getElementsByClassName("day")[i].id = days[i];
 }
